0d45e19a5af5e257c7e166ea8ead642b75aaee45
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-01-30 22:25:38 +0000

    dice refactored

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,4 +1,17 @@
 class DiceController < ApplicationController
+ 
+  def infinity_and_beyond
+    @num_dice=params.fetch("number_of_dice")
+    @dice_sides=params.fetch("how_many_sides")
+
+    @array_of_rolls=Array.new
+
+    @num_dice.to_i.times do
+      @array_of_rolls.push(rand(@dice_sides.to_i)+1)
+    end
+    
+    render({:template => "dice_templates/infinity.html.erb"})
+  end
 
   def one_six
     @array_of_rolls = Array.new
